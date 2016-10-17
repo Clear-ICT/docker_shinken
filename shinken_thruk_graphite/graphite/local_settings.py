392ff17fdd652815e0e42030da5753129683bece
Mike Telahun <miket@clearict.com>
2016-10-17 17:01:14 +0300

    Make graphite able to read the carbon metrics
    It seems wHISPER_DIR is crucial here.

--- shinken_thruk_graphite/graphite/local_settings.py
+++ shinken_thruk_graphite/graphite/local_settings.py
@@ -1,6 +1,7 @@
 STORAGE_DIR = '/var/lib/graphite/storage'
 LOG_DIR = '/var/log/graphite'
+WHISPER_DIR = '/var/lib/graphite/whisper'
+INDEX_FILE = '/var/lib/graphite/storage/index'
 SECRET_KEY = "BonoggyiackVorsItciswacNowvenijhakdempholkojDowWyggofIddokwupoic"
-TIME_ZONE = 'Africa/Addis_Ababa'
+TIME_ZONE = 'UTC'
 USE_REMOTE_USER_AUTHENTICATION = True
-
